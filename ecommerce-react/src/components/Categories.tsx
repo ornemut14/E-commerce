89ef744c4449b4967c3e412f27fa1c6f62400224
Ornella Mut <ornellamut39@gmail.com>
2026-02-27 11:40:02 -0300

    carrusel de categorias

--- ecommerce-react/src/components/Categories.tsx
+++ ecommerce-react/src/components/Categories.tsx
@@ -1,3 +1,9 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+
 import necklace from "../assets/necklace.jpg";
 import ring from "../assets/ring.jpg";
 import earrings from "../assets/earrings.jpg";
@@ -5,26 +11,66 @@ import earrings from "../assets/earrings.jpg";
 function Categories() {
   return (
     <section className="categories">
-      <div
-        className="category-card"
-        style={{ backgroundImage: `url(${necklace})` }}
-      >
-        Necklace
-      </div>
+     <Swiper
+      modules={[Navigation]}
+      navigation
+      loop={true}
+      spaceBetween={0}
+      slidesPerView={1.4}
+      breakpoints={{
+        640: {
+          slidesPerView: 2.6,
+        },
+        1024: {
+          slidesPerView: 3.8,
+        },
+      }}
+    >
+        <SwiperSlide>
+          <div
+            className="category-card"
+            style={{ backgroundImage: `url(${necklace})` }}
+          >
+            Cadenas
+          </div>
+        </SwiperSlide>
 
-      <div
-        className="category-card"
-        style={{ backgroundImage: `url(${earrings})` }}
-      >
-        Earrings
-      </div>
+        <SwiperSlide>
+          <div
+            className="category-card"
+            style={{ backgroundImage: `url(${earrings})` }}
+          >
+            Aros
+          </div>
+        </SwiperSlide>
 
-      <div
-        className="category-card"
-        style={{ backgroundImage: `url(${ring})` }}
-      >
-        Rings
-      </div>
+        <SwiperSlide>
+          <div
+            className="category-card"
+            style={{ backgroundImage: `url(${ring})` }}
+          >
+            Anillos
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div
+            className="category-card"
+            style={{ backgroundImage: `url(${ring})` }}
+          >
+            Pulseras
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div
+            className="category-card"
+            style={{ backgroundImage: `url(${ring})` }}
+          >
+            Dijes
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </section>
   );
 }
